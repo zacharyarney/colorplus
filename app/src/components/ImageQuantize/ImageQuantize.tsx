00fa4cda73fdf4a69493bbd4edb5955cdf4099f6
zacharyarney <zachary.arney@gmail.com>
2020-12-08 23:05:37 -0600

    ImageQuantize creates pixel array when img received

--- app/src/components/ImageQuantize/ImageQuantize.tsx
+++ app/src/components/ImageQuantize/ImageQuantize.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ndarray from 'ndarray';
+import { createPixelArray } from '../../util/imageProcessing';
 
 interface ImageQuantizeProps {
   imageUrl: string;
@@ -9,9 +10,11 @@ export function ImageQuantize(props: ImageQuantizeProps) {
   const [pixelArr, setPixelArr] = useState<ndarray>(ndarray([]));
 
   useEffect(() => {
-
-    setPixelArr(imagePixels);
-  }, [props.imageUrl, pixelArr]);
+    if (props.imageUrl) {
+      const imagePixels = createPixelArray(props.imageUrl);
+      setPixelArr(imagePixels);
+    }
+  }, [props.imageUrl]);
 
   return props.imageUrl.length ? (
     <img src={props.imageUrl} style={{ maxWidth: '800px' }} alt="your image" />
