24053ec370f3143924de7e0386ff24c4e224d367
zacharyarney <zachary.arney@gmail.com>
2021-04-22 17:45:50 -0500

    implements user-selectable palette size for generatePalette function

--- app/src/components/ImageQuantize/ImageQuantize.tsx
+++ app/src/components/ImageQuantize/ImageQuantize.tsx
@@ -3,6 +3,7 @@ import { generatePalette } from '../../util/imageProcessing';
 
 interface ImageQuantizeProps {
   imageUrl: string;
+  paletteSize: string;
 }
 
 export function ImageQuantize(props: ImageQuantizeProps) {
@@ -11,11 +12,13 @@ export function ImageQuantize(props: ImageQuantizeProps) {
   useEffect(() => {
     if (props.imageUrl) {
       // TODO: user should be able to set sampling resolution and palette size
-      generatePalette(props.imageUrl, 10).then(colors => {
-        setColorPalette(colors);
-      });
+      generatePalette(props.imageUrl, Number(props.paletteSize)).then(
+        colors => {
+          setColorPalette(colors);
+        }
+      );
     }
-  }, [props.imageUrl]);
+  }, [props.imageUrl, props.paletteSize]);
 
   const image = props.imageUrl.length ? (
     <img src={props.imageUrl} style={{ maxWidth: '400px' }} alt="upload" />
